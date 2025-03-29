04baea6bb8f47a297f29b08da5122b25e13d67fb
Rafa Cruz <rafael.lourenco.cruz20@gmail.com>
2025-03-29 17:27:00 +0000

    Co-authored-by: JoaoResina <JoaoResina@users.noreply.github.com>

--- paginas/index.php
+++ paginas/index.php
@@ -1,30 +1,32 @@
 <?php
-    include '../basedados/basedados.h';
     session_start();
+    include '../basedados/basedados.h';
 ?>
 
 <!DOCTYPE html>
-<html lang="pt">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<html lang = "en">
+    <meta charset = "UTF-8">
+    <meta name = "viewport" content = "width=device-width, initial-scale = 1.0">
     <link rel="stylesheet" href="index.css">
-    <title>FelixBus - Viaje com Conforto</title>
-</head>
+    <title>FelixBus</title>
+<head>
 <body>
-    <header>
-        <h1>FelixBus - Viaje com Conforto</h1>
-    </header>
-    <div class="container">
-        <div class="hero">
-            <h2>Explore novas rotas com segurança e comodidade!</h2>
-            <p>O seu transporte confiável, sempre à sua disposição.</p>
-            <a href="login.php" class="btn">Login</a>
-            <a href="registar.php" class="btn">Registar</a>
-        </div>
+    <nav>
+    <div class = "logo">
+    <h1>Felix<span>Bus</span></h1>
     </div>
-    <footer>
-        <p>&copy; 2025 <img src="estcb.png" style="max-height: 20px; filter: grayscale(100%);"> João Resina & Rafael Cruz</p>
-    </footer>
+        <div class= "links">
+        <div class = "link"> <a href="">HOME</a></div>
+        <div class = "link"> <a href="">SERVIÇOS</a></div>
+        <div class = "link"> <a href="">CONTACTOS</a></div>
+    </div>
+    <div class="buttons">
+        <div class="btn"><button>Login</button></div>
+        <div class="btn"><button>Sign up</button></div>
+    </div>
+    </nav>
+    <section>
+    </section>
 </body>
+</head>
 </html>
