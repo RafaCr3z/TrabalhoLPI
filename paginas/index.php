0100faefdb60f6a580130b8bc6c09311d306218c
Rafa Cruz <rafael.lourenco.cruz20@gmail.com>
2025-03-30 00:34:30 +0000

    d

--- paginas/index.php
+++ paginas/index.php
@@ -12,20 +12,20 @@
 <head>
 <body>
     <nav>
-    <div class = "logo">
-    <h1>Felix<span>Bus</span></h1>
-    </div>
+        <a href="index.php" class="logo"> <!-- Link adicionado aqui -->
+            <h1>Felix<span>Bus</span></h1>
+        </a>
         <div class= "links">
-        <div class = "link"> <a href="">HOME</a></div>
-        <div class = "link"> <a href="">SERVIÇOS</a></div>
-        <div class = "link"> <a href="">CONTACTOS</a></div>
-    </div>
-    <div class="buttons">
-        <div class="btn"><button>Login</button></div>
-        <div class="btn"><button>Sign up</button></div>
-    </div>
-    </nav>
-    <section>
+            <div class = "link"> <a href="index.php">HOME</a></div>
+            <div class = "link"> <a href="servicos.php">SERVIÇOS</a></div>
+            <div class = "link"> <a href="contactos.php">CONTACTOS</a></div>
+        </div>
+        <div class="buttons">
+            <div class="btn"><a href="login.php"><button>Login</button></a></div>
+            <div class="btn"><a href="registar.php"><button>Registar</button></a></div>
+        </div>
+        </nav>
+<section>
     </section>
 </body>
 </head>
