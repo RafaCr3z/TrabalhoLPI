0df44098ddcf290f71394ce83bc7ecbba7ef0669
Rafa Cruz <rafael.lourenco.cruz20@gmail.com>
2025-04-05 21:31:20 +0100

    da

--- paginas/login.php
+++ paginas/login.php
@@ -84,37 +84,3 @@ mysqli_close($conn);
 </body>
 
 </html>
-=======
-    if (isset($_POST["nome"]) && isset($_POST["pass"])) {
-
-        $nome = $_POST["nome"];
-        $pass = $_POST["pass"];
-
-        $sql = "SELECT * FROM `utilizador` WHERE `nome` = '$nome' AND `password` = '$pass'";
-        $result = mysqli_query($conexao, $sql);
-
-        $row = mysqli_fetch_array($result);
-
-        $id_nivel = $row['id_nivel'];
-        $id_utilizador = $row['id_utilizador'];
-
-        $_SESSION["nome"] = $nome;
-        $_SESSION["id_nivel"] = $id_nivel;
-        $_SESSION["id_utilizador"] = $id_utilizador;
-
-        if (!$result || mysqli_num_rows($result) == 0) {
-        
-            $_SESSION['erro'] = 1;
-            header("Location: login.php");
-            exit();
-        }else if ($id_nivel == 4) {
-            $_SESSION['erro'] = 2;
-            header("Location: login.php");
-            exit();
-        }
-
-        header("Location: home.php");
-        exit();
-    }
-?>
->>>>>>> 3a0a63a66d89e49aa57a9794bb882dcfd3fd04f4
